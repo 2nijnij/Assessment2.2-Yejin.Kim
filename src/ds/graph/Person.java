9aa3f6f87f5073984e8de45d3a9c71c323b72951
Yejin Kim <kimyy049@mymail.unisa.edu.au>
2023-11-16 14:47:24 +1030

    Typo revise

--- src/ds/graph/Person.java
+++ src/ds/graph/Person.java
@@ -78,6 +78,6 @@ public class Person {
 		if (this == o) return true;
 		if (o == null || getClass() != o.getClass()) return false;
 		Person person = (Person) o;
-		return Objects.eqauls(name, person.name);
+		return Objects.equals(name, person.name);
 	}
 }
