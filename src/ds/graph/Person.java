bd1e66ce4bbeab73397029f0f6ec267114b8e498
Yejin Kim <kimyy049@mymail.unisa.edu.au>
2023-11-16 14:23:45 +1030

    add attributes

--- src/ds/graph/Person.java
+++ src/ds/graph/Person.java
@@ -2,6 +2,17 @@ package ds.graph;
 
 import java.util.ArrayList;
 
+/*
+ * This person class have to represent an individual in a social graph
+ * There are attributes in this class
+*/
+public class Person {
+	private float socialHygiene;
+	private int age;
+	private String name;
+	private ArrayList<Person> contacts;
+}
+
 public class Person {
 	
 	public Person(String name, int age, float socialHygine) {
