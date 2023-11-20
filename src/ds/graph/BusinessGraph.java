63aa96105ec341dcca36a0d96433417a7d7f7aff
Yejin Kim <kimyy049@mymail.unisa.edu.au>
2023-11-20 18:42:16 +1030

    void addVertex

--- src/ds/graph/BusinessGraph.java
+++ src/ds/graph/BusinessGraph.java
@@ -7,4 +7,14 @@ public class BusinessGraph {
 	public BusinessGraph() {
 		vertices = new ArrayList<>();
 	}
+	
+	public void addVertex(Business bus) throws IllegalArgumentException {
+		if (bus == null) {
+			throw new IllegalArgumentException("Business cannot be null");
+		}
+		if (!vertices.contains(bus)) {
+			vertices.add(bus);
+		}
+	}
 }
+
