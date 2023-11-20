cddda8839b5e1efaa3c0a75755d89d699421f46d
Yejin Kim <kimyy049@mymail.unisa.edu.au>
2023-11-20 18:22:50 +1030

    void addEdge

--- src/ds/graph/Business.java
+++ src/ds/graph/Business.java
@@ -13,4 +13,21 @@ public class Business {
 		this.name = name;
 		this.edges = new HashMap<>();
 	}
+	
+    /**
+     * Adds an edge from this Business to another via a Person.
+     * If an edge to the destination already exists, only the higher weighted edge is kept.
+     * 
+     * @param dest The destination Business.
+     * @param route The Person representing the edge.
+     */
+    public void addEdge(Business dest, Person route) {
+        if (dest == null || route == null) return;
+
+        Person existingRoute = edges.get(dest);
+        if (existingRoute == null || route.getInfectiveness() > existingRoute.getInfectiveness()) {
+            edges.put(dest, route);
+        }
+    }
+
 }
