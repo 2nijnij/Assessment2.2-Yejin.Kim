e27f2991a91becdfb239a00afa9e4d8ae48ea04d
Yejin Kim <kimyy049@mymail.unisa.edu.au>
2023-11-24 21:05:15 +1030

    testAddVertex method

--- src/ds/graph/tests/BusinessGraphTest.java
+++ src/ds/graph/tests/BusinessGraphTest.java
@@ -14,4 +14,12 @@ public class BusinessGraphTest {
 	void setUp() {
 		bg = new BusinessGraph();
 	}
+	
+	@Test
+	// tests if a new business can be successfully added to the BusinessGraph
+	void testAddvertex() {
+		Business newBusiness = new Business("TechCorp");
+		bg.addVertex(newBusiness);
+		assertTrue(bg.contains(newBusiness), "Business should be added to the graph.");
+	}
 }
